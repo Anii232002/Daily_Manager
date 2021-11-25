9114d35a9d4f5f971450c71a8a23a6ec0f20f304
Umang <umang2singh@gmail.com>
2021-11-26 00:51:44 +0530

    Added ToDo

--- app/src/main/java/com/example/dailymanager/MainActivity.java
+++ app/src/main/java/com/example/dailymanager/MainActivity.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static ArrayList<String> task = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
